fe3b40acc877479850143dfbd13c499bf41b1999
Tope Taiwo <topzyray@yahoo.com>
2024-05-12 20:15:40 +0100

    Update to Repository Detail page

--- src/pages/RepositoryDetails.jsx
+++ src/pages/RepositoryDetails.jsx
@@ -42,20 +42,18 @@ const RepositoryDetails = () => {
     fetchRepo();
   }, []);
 
-  console.log(repo);
-
   return (
-    <div className="w-full mt-6 py-4 lg:py-10 px-6 md:px-10 lg:px-36">
+    <div className="w-full flex flex-col gap-10 mt-6 py-4 lg:py-10 px-6 md:px-10 lg:px-36">
       <div className="flex flex-col gap-5 max-w-3xl mx-auto">
         <h1 className="text-2xl sm:text-3xl md:text-4xl text-center font-semibold leading-tight md:leading-[58px]">
           Repository Details Page
         </h1>
 
         {repo && (
-          <div className="flex flex-col md:flex-row gap-4 h-full">
+          <div className="flex flex-col md:flex-row gap-8 h-full">
             {/* Owner's information */}
             <div>
-              <h3 className="font-semibold text-base sm:text-lg underline">
+              <h3 className="font-semibold text-base sm:text-lg underline mb-3">
                 Owner's Information:
               </h3>
               <div className="flex items-center gap-4">
@@ -120,7 +118,7 @@ const RepositoryDetails = () => {
         )}
       </div>
 
-      <Link to="/" className="hover:underline">
+      <Link to="/" className="underline hover:underline font-semibold mt-6">
         Back to Repo Lists
       </Link>
     </div>
